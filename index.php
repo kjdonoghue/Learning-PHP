4642cd15d7b131eefcc19797db6c7c384feac610
Katy Donoghue <kathrynjdonoghue@gmail.com>
2021-02-18 17:18:53 -0500

    arrays

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
  <?php 
 
-define('NAME', 'Katy');
+// define('NAME', 'Katy');
 
 $stringOne = 'my email is ';
 $stringTwo = 'email@email.com';
 
-$name = "Katy";
+// $name = "Katy";
 
-echo "hey, my name is $name";
+// echo "hey, my name is $name";
 
-echo str_replace('K', 's', $name)
+// echo str_replace('K', 's', $name)
+
+$ages = [20, 30, 40];
+
+$peopleTwo = array('One', 'Two', 'Three');
+
+// echo $peopleTwo[1];
+$ages[] = 60;
+array_push($ages, 70);
+print_r($ages);
+echo count($ages);
+
+$arrayThree =  array_merge($peopleTwo, $ages);
+print_r($arrayThree);
+
+//Key and Value Pairs
+
+$ninjasOne = ['shaun' => 'black', 'mario' => 'orange', 'Luigi' => 'brown'];
+
+echo $ninjasOne['mario'];
+
+$ninjasTwo = array('shaun' => 'black', 'mario' => 'orange', 'Luigi' => 'brown');
+
+print_r($ninjasTwo)
 
 ?> 
 
