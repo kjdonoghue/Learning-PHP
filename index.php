e49ccc5d9dc476b9a1d8c9c59f23d3f062db69e7
Katy Donoghue <kathrynjdonoghue@gmail.com>
2021-02-23 17:06:41 -0500

    added header

--- index.php
+++ index.php
@@ -1,48 +1,15 @@
- <?php 
+<?php
 
-// define('NAME', 'Katy');
 
-$stringOne = 'my email is ';
-$stringTwo = 'email@email.com';
 
-// $name = "Katy";
-
-// echo "hey, my name is $name";
-
-// echo str_replace('K', 's', $name)
-
-$ages = [20, 30, 40];
-
-$peopleTwo = array('One', 'Two', 'Three');
-
-// echo $peopleTwo[1];
-$ages[] = 60;
-array_push($ages, 70);
-print_r($ages);
-echo count($ages);
-
-$arrayThree =  array_merge($peopleTwo, $ages);
-print_r($arrayThree);
-
-//Key and Value Pairs
-
-$ninjasOne = ['shaun' => 'black', 'mario' => 'orange', 'Luigi' => 'brown'];
-
-echo $ninjasOne['mario'];
-
-$ninjasTwo = array('shaun' => 'black', 'mario' => 'orange', 'Luigi' => 'brown');
-
-print_r($ninjasTwo)
-
-?> 
+?>
 
 
 <!DOCTYPE html>
 <html>
-    <head>
-    </head>
-    <body>
+   
+    <?php include('./templates/header.php') ?>
         <h1> <?php echo 'Hello World'; ?></h1>
-        <div> <?php echo $stringOne.$stringTwo; ?></div>
+  
     </body>
 </html>
---
+++ index2.php
@@ -0,0 +1,70 @@
+ <?php 
+
+// // define('NAME', 'Katy');
+
+$stringOne = 'my email is ';
+$stringTwo = 'email@email.com';
+
+// // $name = "Katy";
+
+// // echo "hey, my name is $name";
+
+// // echo str_replace('K', 's', $name)
+
+// $ages = [20, 30, 40];
+
+// $peopleTwo = array('One', 'Two', 'Three');
+
+// // echo $peopleTwo[1];
+// $ages[] = 60;
+// array_push($ages, 70);
+// print_r($ages);
+// echo count($ages);
+
+// $arrayThree =  array_merge($peopleTwo, $ages);
+// print_r($arrayThree);
+
+// //Key and Value Pairs
+
+// $ninjasOne = ['shaun' => 'black', 'mario' => 'orange', 'Luigi' => 'brown'];
+
+// echo $ninjasOne['mario'];
+
+// $ninjasTwo = array('shaun' => 'black', 'mario' => 'orange', 'Luigi' => 'brown');
+
+// print_r($ninjasTwo)
+
+// $blogs = ['one', 'two', 'three', 'four'];
+
+// for ($i = 0; $i < count($blogs); $i++){
+//     echo 'blog';
+// };
+
+// foreach ($blogs as $blog) {
+//     echo "hello";
+// };
+
+$ninjas = ['shaun', 'ryu', 'yoshi'];
+
+// for ($i = 0; $i <count($ninjas); $i++) {
+//     echo $ninjas[$i] . '<br/>';
+// };
+
+// foreach ($ninjas as $ninja) {
+//     echo $ninja . '<br/>';
+// };
+
+
+
+?> 
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h1> <?php echo 'Hello World'; ?></h1>
+        <div> <?php echo $stringOne.$stringTwo; ?></div>
+    </body>
+</html>
